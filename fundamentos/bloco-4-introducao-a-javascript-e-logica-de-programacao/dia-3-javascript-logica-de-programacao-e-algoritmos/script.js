26318c00195d309329f9bc36267c005d09b77d18
Pedro Costa <pedrocostaugusto@gmail.com>
2022-04-08 17:12:25 -0300

    Exercício 4 - Dia 3 - Bloco 4 - Concluído

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -1,15 +1,18 @@
-let array = ['java', 'javascript', 'python', 'html', 'css'];
-let maior = ['', -1];
-let menor = ['', 999999];
+const numLimit = 50;
+let primo = 1;
+let analize = true;
 
-for (var i = 0; i < array.length; i++) {
-    let analysis = array[i].length;
-    if (analysis > maior[1]) {
-        maior[0] = array[i];
-        maior[1] = analysis;
-    } else if (analysis < menor[1]) {
-        menor[0] = array[i];
-        menor[1] = analysis;
+for (var i = 1; i <= numLimit; i++) {
+    for (var j = 2; j < i; j++) {
+        let divisivel = i % j;
+        if (divisivel === 0) {
+            analize = false;
+        }
+    }
+    if (analize) {
+        primo = i;
+    } else {
+        analize = true;
     }
 }
-console.log('A menor palavra encontrada foi ' + menor[0] + ' com ' + menor[1] + ' letras, e a maior palavra encontrada foi ' + maior[0] + ' com ' + maior[1] + ' letras.');
+console.log('O maior número primo encontrado foi: ' + primo);
