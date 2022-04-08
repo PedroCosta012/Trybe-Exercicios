ca3215b1bcfd7883046a6d75a3d1cf4ea6bfccf1
Pedro Costa <pedrocostaugusto@gmail.com>
2022-04-08 16:26:07 -0300

    Exercício 3 - Dia 3 - Bloco 4 - Concluído

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -1,8 +1,15 @@
-let word = 'tryber';
-let wordInvert = '';
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maior = ['', -1];
+let menor = ['', 999999];
 
-for (let i = word.length - 1; i >= 0; i--) {
-    wordInvert += word[i];
+for (var i = 0; i < array.length; i++) {
+    let analysis = array[i].length;
+    if (analysis > maior[1]) {
+        maior[0] = array[i];
+        maior[1] = analysis;
+    } else if (analysis < menor[1]) {
+        menor[0] = array[i];
+        menor[1] = analysis;
+    }
 }
-
-console.log(wordInvert);
+console.log('A menor palavra encontrada foi ' + menor[0] + ' com ' + menor[1] + ' letras, e a maior palavra encontrada foi ' + maior[0] + ' com ' + maior[1] + ' letras.');
